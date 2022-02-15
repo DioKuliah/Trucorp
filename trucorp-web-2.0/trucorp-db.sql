b4af04e4a81c7c68dad619571220ba13a745ef06
DioKuliah <99707661+DioKuliah@users.noreply.github.com>
2022-02-15 11:38:36 +0700

    Add files via upload

---
+++ trucorp-web-2.0/trucorp-db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+	ID INT NOT NULL PRIMARY KEY,
+	Nama VARCHAR(255),
+	Alamat VARCHAR(255),
+	Jabatan VARCHAR(255)
+);
+
+INSERT INTO users VALUES(1, 'Claudio', 'Jakarta', 'Mahasiswa');
